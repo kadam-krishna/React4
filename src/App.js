301d9e46fc214f9e84bf43cdacd3c51c39b52b34
Krishna <krishnakadamwork@gmail.com>
2023-10-14 16:15:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+import React,{ useEffect ,useState} from 'react';
 import './App.css';
-
 function App() {
+  const [userData,setData] = useState([]);
+  useEffect(()=>{
+    fetch("https://dummyjson.com/users")
+      .then((res) => res.json())
+      .then((data) => setData(data.users));
+  }, []);
+const dummy=()=>
+{
+  return userData.map((user) => (
+    <tr>
+      <td>{user.id}</td>
+      <td>
+        <img src={user.image} alt={user.firstName} width={30} />
+      </td>
+      <td>{user.firstName}</td>
+      <td>{user.lastName}</td>
+      <td>{user.gender}</td>
+      <td>{user.email}</td>
+      <td>{user.username}</td>
+      <td>{user.domain}</td>
+      <td>{user.ip}</td>
+      <td>{user.university}</td>
+    </tr>
+  ))
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='container'> 
+        <table className='table' border={1} width={'100%'}>
+          <thead>
+          <tr>
+              <th>SNo</th>
+              <th>Profile</th>
+              <th>FirstName</th>
+              <th>LastName</th>
+              <th>Gender</th>
+              <th>E-mail</th>
+              <th>Username</th>
+              <th>Domain</th>
+              <th>IP</th>
+              <th>University</th>
+            </tr>
+          </thead>
+          <tbody>
+            {dummy()}
+          </tbody>
+        </table>
+      </div>
   );
 }
 
